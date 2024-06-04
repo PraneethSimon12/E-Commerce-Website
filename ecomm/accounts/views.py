e7ab16837eebd5969090ccb279fba9ee16d1e0c2
PraneethSimon12 <praneethsimon2@gmail.com>
2024-06-04 14:26:28 +0530

    fec

--- ecomm/accounts/views.py
+++ ecomm/accounts/views.py
@@ -2,10 +2,33 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.contrib import messages
 from django.contrib.auth.models import User
+from django.contrib.auth.models import authenticate , login , logout
 
 # Create your views here.
 
 def login_page(request):
+
+    if request.method=='POST':
+
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        user_obj = User.objects.filter(username = email)
+
+        if not user_obj.exists():
+            messages.warning(request,'Account not found')
+            return HttpResponseRedirect(request.path_info)
+        
+        user_obj = authenticate(username = email , password = password)
+        if user_obj:
+            login(request , user_obj)
+            return HttpResponseRedirect('/')
+
+        messages.success(request,'Invalid credentials')
+        return HttpResponseRedirect(request.path_info)
+
     return render(request,'accounts/login.html')
 
 def register_page(request):
@@ -27,7 +50,7 @@ def register_page(request):
         user_obj.set_password(password)
         user_obj.save()
 
-        messages.warning(request,'An email has been sent on your mail.')
+        messages.success(request,'An email has been sent on your mail.')
         return HttpResponseRedirect(request.path_info)
 
     return render(request,'accounts/register.html')
