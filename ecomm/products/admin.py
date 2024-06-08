96ba5b604a56f4020ccf76101f5740310f8e3a63
PraneethSimon12 <praneethsimon2@gmail.com>
2024-06-08 15:12:55 +0530

    dfv

--- ecomm/products/admin.py
+++ ecomm/products/admin.py
@@ -6,6 +6,13 @@ from .models import *
 
 admin.site.register(Category)
 
-admin.site.register(Product)
+class ProductImageAdmin(admin.StackedInline):
+    model = ProductImage
+
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [ProductImageAdmin]
+
+admin.site.register(Product,ProductAdmin)
 
 admin.site.register(ProductImage)
+
