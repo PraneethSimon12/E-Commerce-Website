dbd00e2b1f1dddf3ef98e039abd827462028e724
PraneethSimon12 <praneethsimon2@gmail.com>
2024-06-08 11:23:05 +0530

    jml,

--- ecomm/products/models.py
+++ ecomm/products/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from base.models import *
 from django.utils.text import slugify
+#slugify used for creating a URL friendly version of a string by converting it into lowercase annd replacing spaces with hyphens.
 
 # Create your models here.
 
@@ -10,8 +11,12 @@ class Category(BaseModel):
     category_image = models.ImageField(upload_to="categories")
 
     def save(self , *args , **kwargs):
-        self.slug = slugify(self.title)
+        self.slug = slugify(self.category_name)
+        #slug field is
         super(Category,self).save(*args,**kwargs)
+
+    def __str__(self)->str:
+        return self.category_name
 
 
 class Product(BaseModel):
@@ -21,6 +26,14 @@ class Product(BaseModel):
     category = models.ForeignKey(Category, on_delete=models.CASCADE,related_name= "products")
     price = models.IntegerField()
     product_desciption = models.TextField()
+
+
+    def save(self , *args, **kwargs):
+        self.slug = slugify(self.product_name)
+        super(Product,self).save(*args,**kwargs)
+
+    def __str__(self) -> str:
+        return self.product_name
 
 class ProductImage(BaseModel):
     product = models.ForeignKey(Product,on_delete=models.CASCADE,related_name="product_images")
